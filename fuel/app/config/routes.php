c28a09d43eb180dfaf4b44f98d9899c28923d1d0
yuma iwasaki <iwasakiyuma@gmail.com>
2014-07-12 12:05:20 +0900

    とりあえずルート設定

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,7 +1,7 @@
 <?php
 return array(
-	'_root_'  => 'welcome/index',  // The default route
+	'_root_'  => 'event/index',  // The default route
 	'_404_'   => 'welcome/404',    // The main 404 route
-	
+	'event'   => 'welcome/index',
 	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
 );
