fb2d0f2f42f8fb8281ca58ffa5cd20297cffcb60
yuma iwasaki <iwasakiyuma@gmail.com>
2014-07-21 18:18:02 +0900

    DoorkeeperのAPIからイベントの取得処理追加

---
+++ fuel/app/tasks/doorkeeper.php
@@ -0,0 +1,235 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.7
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+namespace Fuel\Tasks;
+
+/**
+ * Robot example task
+ *
+ * Ruthlessly stolen from the beareded Canadian sexy symbol:
+ *
+ *    Derek Allard: http://derekallard.com/
+ *
+ * @package   Fuel
+ * @version   1.0
+ * @author    Phil Sturgeon
+ */
+
+ini_set("mbstring.internal_encoding", "UTF-8");
+
+class Doorkeeper 
+{
+
+  /**
+   * This method gets ran when a valid method name is not used in the command.
+   *
+   * Usage (from command line):
+   *
+   * php oil r robots
+   *
+   * or
+   *
+   * php oil r robots "Kill all Mice"
+   *
+   * @return string
+   */
+  public static function run($speech = null)
+  {
+      static::exec();
+  }
+  private static function exec()
+  {
+      $data = static::get_zusaar();
+      // print_r($data);
+      $valid_data = array();
+      $cnt = 0;
+      $enabled_keys = array('id',
+                          'title',
+                          'public_url',
+                          'starts_at',
+                          'ends_at',
+                          'address',
+                          'participants',
+                          'ticket_limit',
+                          'waitlisted',
+                          'venue_name',
+                          'lon',
+                          'long',
+                        );
+
+      // data to event
+      foreach ($data as $event) {
+          //$limit = $event['owner_nickname'];
+          //$limit = $event['url'];
+          $data_array = array();
+          // イベントから情報を抜き出し
+          foreach ($event['event'] as $key => $value) {
+              if(in_array($key, $enabled_keys))
+              {
+                  $data_array = array_merge(array($key => $value),$data_array);
+              }
+          }
+          array_push($valid_data,$data_array);
+      }
+      $zusaar_data = static::convert_zusaar_data($valid_data);
+      //print_r($zusaar_data);
+      $entry_data = static::convert_now_date($zusaar_data);
+      $entry_data = static::input_default_value($entry_data);
+      // print_r($entry_data);
+      $db = \Database_Connection::instance();
+      $db->start_transaction();
+      try
+      {
+        // 全件、削除
+        \DB::delete('events')->where('type_flg', 3)->execute();
+        // 新規行の登録
+        foreach ($entry_data as $event) {
+            \Model_Event::forge($event)->save();
+        }
+        $db->commit_transaction();
+      }catch(\Exception $ex)
+      {
+          $db->rollback_transaction();
+          print_r($ex);
+      }
+  }
+  private static function input_default_value($data)
+  {
+      $events = array();
+      foreach ($data as $event) {
+          if(empty($event['limit']))
+          {
+              $event['limit'] = 0;
+          }
+          array_push($events,$event);
+      }
+      return $events;
+  }
+  private static function convert_now_date($data)
+  {
+      $today = Date('Ymd')."\n";
+      $entry_data = array();
+      foreach ($data as $event) {
+          $ended_at = Date('Ymd',strtotime($event['ended_at']));
+          // 今日以上の日付のデータのみ抽出
+          if($today <= $ended_at)
+          {
+              array_push($entry_data,$event);
+          }
+      }
+      return $entry_data;
+  }
+  // 配列のKeyを変換    
+  private static function convert_zusaar_data($data)
+  {
+      $from_keys = array('id',
+                        'title',
+                        'public_url',
+                        'starts_at',
+                        'ends_at',
+                        'address',
+                        'participants',
+                        'ticket_limit',
+                        'waitlisted',
+                        'venue_name',
+                        'lon',
+                        'long',
+                        );
+      $to_keys = array( 'event_id',
+                        'title',
+                        'url',
+                        'started_at',
+                        'ended_at',
+                        'address',
+                        'accepted',
+                        'limit',
+                        'waiting',
+                        'place',
+                        'lon',
+                        'lat',
+                        );
+      $convert_array = array();
+      foreach ($data as $event) {
+          $event_array = array();
+          for($i=0;$i<count($from_keys);$i++)
+          {
+              if(array_key_exists($from_keys[$i],$event))
+              {
+                  $event_array = array_merge($event_array,array($to_keys[$i] => $event[$from_keys[$i]]));
+
+              }
+          }
+          $event_array = array_merge($event_array,array('type_flg'=> 3));
+          array_push($convert_array,$event_array);
+      }
+      return $convert_array;
+  }
+  
+  private static function get_zusaar()
+  {
+      // 何ヶ月取得するのか。
+      $zusaar_data = array();
+      $zusaar_data = array_merge($zusaar_data,static::get_zusaar_api_all());
+      return $zusaar_data;
+  }
+
+  private static function get_zusaar_api_all($start=1)
+  {
+      $zusaar_data = static::get_zusaar_api($start);
+      // print_r($zusaar_data[0]['event']['starts_at']);
+      // date('Y-m-d H:i:s', strtotime("+ 9 hour"));
+      // print date("Ymd")."\n";
+      // print date("Ymd",strtotime($zusaar_data[0]['event']['starts_at']))."\n";
+      if(date("Ymd") <= date("Ymd",strtotime($zusaar_data[0]['event']['starts_at'])))
+      {
+          //再帰処理  
+          $zusaar_data = array_merge($zusaar_data,static::get_zusaar_api_all($start+1));
+      }
+      return $zusaar_data;
+  }
+
+  private static function get_zusaar_api($start)
+  {
+      $curl = curl_init();
+      $url = 'http://api.doorkeeper.jp/events?locale=jp&sort=starts_at'. '&page='. $start;
+      print $url."\n";
+      curl_setopt($curl, CURLOPT_URL,$url);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+      $res = curl_exec($curl);
+      $header = curl_getinfo($curl);
+      // APIのHTTPコードをチェック
+      $code = $header["http_code"];
+      if ($code >= 400) { // もしエラーなら
+          header("HTTP", true, $code);
+          echo $res;   // APIのエラー表示をそのままブラウザへ返す(開発作業中はそうしておくと便利)
+          exit();
+      }
+      $data = json_decode($res, true);
+      return $data;
+  }
+
+  /**
+   * An example method that is here just to show the various uses of tasks.
+   *
+   * Usage (from command line):
+   *
+   * php oil r robots:protect
+   *
+   * @return string
+   */
+  public static function protect()
+  {
+
+  }
+}
+
+/* End of file tasks/robots.php */
